ade04c6725260ea21b549833984ce9e5a686cc02
escoladeestudantes <escoladeestudantes@gmail.com>
2021-01-08 14:02:49 -0300

    modified:   03_Train_HOG_SVM_Mask_V/01_Dlib_train.py

--- 03_Train_HOG_SVM_Mask_V/01_Dlib_train.py
+++ 03_Train_HOG_SVM_Mask_V/01_Dlib_train.py
@@ -2,7 +2,7 @@
 
 import dlib
 options = dlib.simple_object_detector_training_options()
-options.add_left_right_image_flips = False
+options.add_left_right_image_flips = True #Mascara simetrica
 options.num_threads = 4
 options.be_verbose = True
 options.C = 5
